d666e116e665b7fb3238f52d2de02cd9cf1935e4
ilatyphi95 <Ilatyphi95@gmail.com>
2022-06-21 21:53:45 +0100

    Implement home screen design

--- src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -28,7 +28,9 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Search
@@ -82,6 +84,7 @@ fun AlignYourBodyElement(
 ) {
     // Implement composable here
     Column(
+        modifier,
         horizontalAlignment = CenterHorizontally
     ) {
         Image(
@@ -193,6 +196,15 @@ fun HomeSection(
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
     // Implement composable here
+    Column(
+        modifier
+            .verticalScroll(rememberScrollState())
+            .padding(vertical = 16.dp)
+    ) {
+        SearchBar(Modifier.padding(horizontal = 16.dp))
+        HomeSection(title = R.string.align_your_body) { AlignYourBodyRow() }
+        HomeSection(title = R.string.favorite_collections) { FavoriteCollectionsGrid() }
+    }
 }
 
 // Step: Bottom navigation - Material
@@ -205,6 +217,7 @@ private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
 @Composable
 fun MySootheApp() {
     // Implement composable here
+    MySootheTheme { HomeScreen() }
 }
 
 private val alignYourBodyData = listOf(
